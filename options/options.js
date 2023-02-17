dbf314e891041c8dfb44c665d13510dfbb8c0de7
Crowdbotics <team@crowdbotics.com>
2023-02-17 23:48:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "steep-smoke-38997",
+  url: "https://steep-smoke-38997.botics.co",
+  api: "https://steep-smoke-38997.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
